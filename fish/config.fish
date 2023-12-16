9f8456e505ce1afde34e67dca0d1278a8777c94e
andresfavio <favioandresromeropozo636@gmail.com>
2023-12-16 14:55:31 -0400

    fish(#1)

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+
+end
+
+alias vi="nvim"
+alias cl="clear"
+alias q="exit"
+alias ls="exa --icons"
+alias ll="exa --icons --header --long --git"
+alias tree="exa --icons --tree"
