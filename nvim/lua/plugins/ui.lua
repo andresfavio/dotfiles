305c7b6544a8c9bcdffd80b7fdff5d984e8ef4bf
andresfavio <favioandresromeropozo636@gmail.com>
2023-12-19 18:44:02 -0400

    nvim(#10)

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -626,19 +626,62 @@ return {
 
 	{
 		"nvimdev/dashboard-nvim",
-		event = "VimEnter",
-		opts = function(_, opts)
+		opts = function()
 			local logo = [[
-   █████╗ ███╗   ██╗██████╗ ██████╗ ███████╗███████╗
-  ██╔══██╗████╗  ██║██╔══██╗██╔══██╗██╔════╝██╔════╝
-  ███████║██╔██╗ ██║██║  ██║██████╔╝█████╗  ███████╗
-  ██╔══██║██║╚██╗██║██║  ██║██╔══██╗██╔══╝  ╚════██║
-  ██║  ██║██║ ╚████║██████╔╝██║  ██║███████╗███████║
-  ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝
-      ]]
+	                                                                      
+	       ████ ██████           █████      ██                     
+	      ███████████             █████                             
+	      █████████ ███████████████████ ███   ███████████   
+	     █████████  ███    █████████████ █████ ██████████████   
+	    █████████ ██████████ █████████ █████ █████ ████ █████   
+	  ███████████ ███    ███ █████████ █████ █████ ████ █████  
+	 ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+  ]]
 
-			logo = string.rep("\n", 6) .. logo .. "\n\n"
-			opts.config.header = vim.split(logo, "\n")
+			logo = string.rep("\n", 5) .. logo .. "\n\n"
+
+			local opts = {
+				theme = "doom",
+				hide = {
+					statusline = false,
+				},
+				config = {
+					header = vim.split(logo, "\n"),
+      -- stylua: ignore
+      center = {
+        { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
+        { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
+        { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+        { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+        { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+      },
+					footer = function()
+						local stats = require("lazy").stats()
+						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+						return {
+							"⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
+						}
+					end,
+				},
+			}
+
+			for _, button in ipairs(opts.config.center) do
+				button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+				button.key_format = "  %s"
+			end
+
+			-- close Lazy and re-open when the dashboard is ready
+			if vim.o.filetype == "lazy" then
+				vim.cmd.close()
+				vim.api.nvim_create_autocmd("User", {
+					pattern = "DashboardLoaded",
+					callback = function()
+						require("lazy").show()
+					end,
+				})
+			end
+
+			return opts
 		end,
 	},
 }
